ad5b7611a42b60b1d4b06781caa0250f4cdce999
mkozien <marek.kozien@gmail.com>
2022-02-17 02:08:25 +0100

    initial steps, navbar and routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { BooksComponent } from './books/books.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ShopComponent } from './shop/shop.component';
+import { TasksComponent } from './tasks/tasks.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/main-page', pathMatch: 'full'},
+  {path: 'main-page', component: MainPageComponent},
+  {path: 'tasks-list', component: TasksComponent},
+  {path: 'shopping-list', component: ShopComponent},
+  {path: 'books-list', component: BooksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { ShopComponent } from './shop/shop.component';
+import { BooksComponent } from './books/books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainPageComponent,
+    TasksComponent,
+    ShopComponent,
+    BooksComponent
   ],
   imports: [
     BrowserModule,
